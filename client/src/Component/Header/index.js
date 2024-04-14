f542f4662225e62ac310c2e8e3a35ae884cbe75f
mr-optimizer <sktovisit@gmail.com>
2024-04-14 15:42:52 +0530

    basic ui for home page

--- client/src/Component/Header/index.js
+++ client/src/Component/Header/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 
 const NavBar = () => {
   const [roomId, setRoomId] = useState("0");
   const [userId, setUserId] = useState("0");
+  const navigate = useNavigate();
+
   return (
     <div className="navbar">
-      <h1 className="company_logo">Descriptionary</h1>
+      <h1 onClick={()=> navigate("/") } className="company_logo">Descriptionary</h1>
       <div>
         <div className="nav_ids">
           Room ID: <span>1abcd34</span>
--- client/src/Component/Landing/index.js
+++ client/src/Component/Landing/index.js
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./style.css";
 
 const Landing = ({ socket }) => {
   const [message, setMessage] = useState("");
+  const navigate = useNavigate();
   const sendHi = () => {
     // Send message to server. We can't specify who we send the message to from the frontend. We can only send to server. Server can then send message to rest of users in room
-    socket.emit('send_message', { message: "HI buddy" });
-  }
-  useEffect(()=> {
+    socket.emit("send_message", { message: "HI buddy" });
+  };
+
+  const createPrivateRoom = (e) => {
+    e.preventDefault();
+    navigate("/room");
+  };
+  useEffect(() => {
     socket.on("rev_msg", (data) => {
       setMessage(data.message);
     });
   }, [socket]);
   return (
     <div className="landing">
-      <div className="profile_container_left"></div>
-      <div></div>
+      <div className="landing_form">
+        <div className="landing_form_title">Welcome to the xyz world!!</div>
+        <input className="landing_form_input" placeholder="Enter your Name" />
+        <div className="landing_form_button_container">
+          <button className="landing_form_button">Go Global</button>
+          <button onClick={createPrivateRoom} className="landing_form_button">
+            Personal Room
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
